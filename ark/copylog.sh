eee75af702463cfcd52081ec26a2ac07be1e7868
Burrows <zburrows@itron.com>
2016-07-18 16:04:34 -0700

    Start

---
+++ ark/copylog.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp /home/ark/ShooterGame/Saved/Logs /home/ark
---
+++ ark/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo ./home/ark/ShooterGame/Binaries/Linux/start.sh TheIsland?listen?SessionName=dickbuttFTW?ServerPassword=dickbutt?ServerAdminPassword=hondaktm -server -log -servergamelog -usecache
