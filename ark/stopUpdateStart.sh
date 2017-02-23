2b729bed71cbab750e9088aac6e1cd651187d2da
ZacharyBurrows <zburrows7@gmail.com>
2017-02-22 17:36:55 -0800

    working on supporting remotely running stopUpdateStart.sh

--- ark/stopUpdateStart.sh
+++ ark/stopUpdateStart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo ./stop.sh
+echo dickbutt | sudo -S /home/ark/epicsrvrmgr/ark/stop.sh
 
 sudo chown -R ark: /home/ark/*
 
 sudo chown -R ark: /home/ark/*
 
-/home/ark/arkserver update
+sudo -u ark /home/ark/arkserver update
 
 nohup /home/ark/epicsrvrmgr/ark/start.sh &
